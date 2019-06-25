b26204fb25c36dcd143889616c3440cbf860428c
Jacky Kaub <jkaub@outlook.fr>
2019-06-25 11:47:43 +0200

    Update MapFranceD3.js

--- web-app/src/components/MapFrance/MapFranceD3.js
+++ web-app/src/components/MapFrance/MapFranceD3.js
@@ -25,10 +25,23 @@ const plotMapFrance = (data, geojson, selectedyear, selectedDataType, node, widt
         svg.firstChild.remove();
     }
 
+    function selecColor(selectedDataType) {
+        switch(selectedDataType) {
+          case 'Excedent':
+              return ['#006aff', '#4190ff', '#7cb2ff', '#b8d5ff', '#ffffff', '#ffc3c3', '#ff9494', '#ff5555', '#ff0000']
+          default:
+            return ['#d4eac7', '#c6e3b5', '#b7dda2', '#a9d68f', '#9bcf7d', '#8cc86a', '#7ec157', '#77be4e', '#70ba45', '#65a83e', '#599537', '#4e8230', '#437029', '#385d22', '#2d4a1c', '#223815'];
+        }
+      }
+
+
+
+
     const deps = svg.append("g");
     //const colors = ['#d4eac7', '#c6e3b5', '#b7dda2', '#a9d68f', '#9bcf7d', '#8cc86a', '#7ec157', '#77be4e', '#70ba45', '#65a83e', '#599537', '#4e8230', '#437029', '#385d22', '#2d4a1c', '#223815'];
-    const colors = ['#006aff', '#4190ff', '#7cb2ff', '#b8d5ff', '#ffffff', '#ffc3c3', '#ff9494', '#ff5555', '#ff0000'];
+    //const colors = ['#006aff', '#4190ff', '#7cb2ff', '#b8d5ff', '#ffffff', '#ffc3c3', '#ff9494', '#ff5555', '#ff0000'];
 
+    const colors = selecColor(selectedDataType)
     const plot = {};
     plot.year = selectedyear;
     plot.dataType = selectedDataType;
